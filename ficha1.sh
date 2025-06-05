de718d21f13617cfd2377610e0462d26d2f445f4
Santiago <santiagodias238@gmail.com>
2025-06-05 18:36:07 +0100

    Update ficha1.sh

--- ficha1.sh
+++ ficha1.sh
@@ -1,7 +1,5 @@
-sudo touch var/log/logs.txt 
-find /etc /var/log /home >> logs.txt 
-last | grep "$(date '+%b %_d')" >> logs.txt # apenas hoje
-last | grep "$(date -d 'yesterday' '+%b %_d')" >> logs.txt  # apenas ontem
+sudo touch var/log/logs.txt   #Criar o ficheiro 
+find /etc /var/log /home >> logs.txt   #Encontrar ficheiros em /etc /var/log e /home que foram modificados nas ultimas 24 horas
+last -s -1day >> logs.txt  #Listar os users que fizeram login no sistema á até 24 horas atras
 
-
-echo "Ficheiro criado em var/log com informacoes de log e de ficheiros modificados"
+echo "Ficheiro criado em var/log com informacoes de log e de ficheiros modificados"  #Informar o utilizador que o ficheiro foi criado
