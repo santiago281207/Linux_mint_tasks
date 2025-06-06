a84a60ed2b886c6b85dae6cf5e9eefb47d018c41
Santiago <santiagodias238@gmail.com>
2025-06-06 20:35:21 +0100

    Update ficha1.sh

--- ficha1.sh
+++ ficha1.sh
@@ -1,5 +1,8 @@
-sudo touch var/log/logs.txt   #Criar o ficheiro 
-find /etc /var/log /home -type f -mtime -1 >> logs.txt   #Encontrar ficheiros em /etc /var/log e /home que foram modificados nas ultimas 24 horas
-last -s -1day >> logs.txt  #Listar os users que fizeram login no sistema á até 24 horas atras
+sudo touch modified_files.txt   #Criar o ficheiro 
+sudo touch user_logs.txt
+echo "" > user_logs.txt
+echo "" > modified_files.txt
+sudo find /etc /var/log /home -type f -mtime -1 >> modified_files.txt   #Encontrar ficheiros em /etc /var/log e /home que foram modificados nas ultimas 24 horas
+sudo  last -s -1day >> user_logs.txt  #Listar os users que fizeram login no sistema á até 24 horas atras
 
-echo "Ficheiro criado em var/log com informacoes de log e de ficheiros modificados"  #Informar o utilizador que o ficheiro foi criado
+echo "Ficheiros modified_files.txt criado.\nFicheiro user_logs.txt criado!\n"  #Informar o utilizador que o ficheiro foi criado
